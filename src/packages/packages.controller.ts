77bd3cf413cf4e77b8abc5940e411353fcb6e72c
Nadim Nahle <nahlenadim@gmail.com>
2026-08-15 07:19:15 +0300

    security(packages): protect package mutation routes

--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -4,22 +4,27 @@ import { Controller, Post, Body, Get, Delete, Param } from '@nestjs/common';
 import { PackagesService } from './packages.service';
 import { CreatePackageDto } from './create-package.dto';
 import { Package } from './package.entity';
+import { Public } from '../auth-validation/public.decorator';
+import { Roles } from '../auth-validation/roles.decorator';
 
 @Controller('packages')
 export class PackagesController {
   constructor(private readonly packagesService: PackagesService) {}
 
   @Post()
+  @Roles('admin')
   async create(@Body() createPackageDto: CreatePackageDto): Promise<Package> {
     return this.packagesService.createPackage(createPackageDto);
   }
 
   @Get()
+  @Public()
   async getAllPackages(): Promise<Package[]> {
     return this.packagesService.getAllPackages();
   }
 
   @Delete(':id')
+  @Roles('admin')
   async deletePackage(@Param('id') id: string): Promise<{ message: string }> {
     await this.packagesService.deletePackage(id);
     return { message: `Package with ID ${id} has been deleted.` };
